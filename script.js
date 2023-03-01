a5295247757f9f3a61415567442e3094d034ded4
nurakbarova <nurayakbarova76@gmail.com>
2023-03-01 06:30:25 +0400

    task

---
+++ script.js
@@ -0,0 +1,19 @@
+let counter =0;
+ let a=3
+let cem =0;
+for(let i=1;i<=0;i++);
+ 
+    if(a % counter == 0 && a%a ==0);{ 
+
+    if(a%2==0) {
+  cen+=i;
+    }
+    }
+
+if(counter>0)
+{
+    console.log("sade ")
+}
+else{
+    console.log("murekkeb")
+}
